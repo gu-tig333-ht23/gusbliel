aedd960fe04a99667873658e58e8fd4c2b6d4264
Dyrker <elmer.blixt@gmail.com>
2023-09-08 14:43:50 +0200

    Städat och laggt till ny view
    Har även tagit bort den där konstiga state klassen, kanske kommer få ångra det sen hehe

--- lib/main.dart
+++ lib/main.dart
@@ -8,73 +8,29 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePageView(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class HomePageView extends StatelessWidget {
  
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: const Color.fromARGB(255, 189, 188, 188),
-        
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         centerTitle: true,
         title: Text("TIG333 TODO"),
         actions: [
@@ -83,16 +39,67 @@ class _MyHomePageState extends State<MyHomePage> {
         
       ),
       body: ListView(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         children: [
-          TaskListItem(taskText: "task 1"),
-          TaskListItem(taskText: "task 2")
+          TaskListItem(taskText: "Slänga sopor"),
+          TaskListItem(taskText: "Handla smör"),
+          TaskListItem(taskText: "Erövra Paraguay"),
+          TaskListItem(taskText: "Vattna blommor")
         ]
         
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => AddTaskView()));
+        },
+        
+        backgroundColor: const Color.fromARGB(255, 189, 188, 188),
+        child: Icon(Icons.add),
       ),
     );
   }
 }
 
+class AddTaskView extends StatelessWidget {
+ 
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        backgroundColor: const Color.fromARGB(255, 189, 188, 188),
+        
+        centerTitle: true,
+        title: Text("TIG333 TODO"),
+        actions: [
+          IconButton(icon: Icon(Icons.more_vert), onPressed: (null))
+        ]
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter task name',
+              ),
+            ),
+            SizedBox(height: 20.0),
+            TextButton(
+              onPressed: () {
+                
+              },
+              style: TextButton.styleFrom(
+                textStyle: TextStyle(color: Colors.black),
+
+              ),
+              child: Text('+ ADD', style:
+                TextStyle(color: Colors.black),),
+            ),
+        ]
+       )
+     )
+    );
+  }
+}
 
